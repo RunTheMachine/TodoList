367fba9648bc1d0efff631510d4b2f0300449b0d
TheBadRobot <45450501+TheBadRobot@users.noreply.github.com>
2019-04-27 15:20:58 +0200

    mise en place de l'environnement

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoList,
   }
 }
 </script>
 
 <style>
+*{
+  box-sizing:border-box;
+}
+.container{
+  max-width:600px;
+  margin:0 auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
