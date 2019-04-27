48a5b3964e7340ab3262b36a3d0b009a3e0af1dd
TheBadRobot <45450501+TheBadRobot@users.noreply.github.com>
2019-04-27 15:54:12 +0200

    Ajout de tache ok et ajout d'une croix en format texte simple pour preparer à la fermeture

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        Todo List goes here
+      <input type="text" class="todo-input" placeholder="What need to be done" v-model="newTodo" @keyup.enter="addTodo">
+      <div v-for="todo in todos" :keys="todo.id" class="todo-item">  
+          <div>
+            {{todo.title}} 
+          </div>
+          <div class="remove-item">
+            X
+          </div>
+      </div>
     </div>
 </template>
 
@@ -9,7 +17,35 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      idForTodo: 3,
+      todos:[
+        {
+          'id': 1,
+          'title': 'Finish Vue Screencast',
+          'completed' : false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over',
+          'completed' : false,
+        },
+      ]
+    }
+  },
+  methods:{
+    addTodo(){
+      if(this.newTodo.trim().length == 0){
+        return
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed:false,
+      })
+
+      this.newTodo = ''
+      this.idForTodo++
     }
   }
 }
@@ -17,18 +53,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
-  font-weight: normal;
+.todo-input{
+  width:100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom:16px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.todo-item{
+  margin-bottom:12px;
+  display:flex;
+  align-items:center;
+  justify-content: space-between;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.remove-item{
+  cursor: pointer;
+  
 }
-a {
-  color: #42b983;
-}
+
 </style>
